2d43dfb804d9faab6034b08f7792f355b9d954fc
Ioannis Papavasileiou <giannis123@gmail.com>
2017-09-27 15:26:25 -0400

    Add files via upload

---
+++ findNotReferenced.py
@@ -0,0 +1,28 @@
+
+import sys
+from bibtexparser import load
+from bibtexparser.bwriter import BibTexWriter
+from bibtexparser.bibdatabase import BibDatabase
+
+
+if len(sys.argv)<4:
+    print('Usage: python findNotReferenced <bibliography.bib> <paper.tex> <notReferenced.bib>')
+    sys.exit(1)
+
+bibFileName = sys.argv[1]
+texFileName = sys.argv[2]
+outFileName = sys.argv[3]
+with open(bibFileName) as bibtex:
+    bibDB = load(bibtex)
+
+with open(texFileName) as texFile:
+    text = ''.join(texFile.readlines())
+
+toInclude = [entry for entry in bibDB.entries if entry['ID'] not in text]
+bibDB.entries = toInclude
+print('found %d bibliography entries not cited in %s'%(len(toInclude),texFileName))
+
+writer = BibTexWriter()
+with open(outFileName, 'w') as bibfile:
+    bibfile.write(writer.write(bibDB))
+print('wrote %d bibliography entries in %s\n'%(len(toInclude),outFileName))
